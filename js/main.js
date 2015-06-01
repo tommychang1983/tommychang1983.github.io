9586789dcacc5639e8f701a496d17cb395a4827e
qichang <changqi@wespoke.com>
2015-06-01 22:09:26 +0800

    update

--- js/main.js
+++ js/main.js
@@ -76,22 +76,13 @@ $(document).ready(function () {
                     initialDelay: 2000,
                     autoStart: false
                 });
-            $(".intro6-0")
-                .textillate({
-                    in: {
-                        effect: 'fadeInLeft',
-                        delay: 200
-                    },
-                    initialDelay: 0,
-                    autoStart: false
-                });
             $(".intro7-0")
                 .textillate({
                     in: {
                         effect: 'fadeInLeft',
                         delay: 200
                     },
-                    initialDelay: 2000,
+                    initialDelay: 500,
                     autoStart: false
                 });
             $(".intro7-1")
@@ -100,35 +91,156 @@ $(document).ready(function () {
                         effect: 'fadeInLeft',
                         delay: 200
                     },
-                    initialDelay: 3000,
+                    initialDelay: 1500,
                     autoStart: false
                 });
 
-            $(".intro8-0")
-                .textillate({
-                    in: {
-                        effect: 'fadeIn',
-                        delay: 200
-                    },
-                    initialDelay: 500,
-                    autoStart: false
-                });
+            //$(".intro8-0")
+            //    .textillate({
+            //        in: {
+            //            effect: 'fadeIn',
+            //            delay: 200
+            //        },
+            //        initialDelay: 500,
+            //        autoStart: false
+            //    });
             $(".intro8-1")
                 .textillate({
                     in: {
                         effect: 'fadeIn',
-                        delay: 200
+                        delay: 150
                     },
-                    initialDelay: 1500,
+                    initialDelay: 500,
                     autoStart: false
                 });
             $(".intro9-0")
                 .textillate({
                     in: {
                         effect: 'fadeIn',
-                        delay: 200
+                        delay: 150
+                    },
+                    initialDelay: 2500,
+                    autoStart: false
+                });
+            //$(".intro10-0")
+            //    .textillate({
+            //        in: {
+            //            effect: 'fadeIn',
+            //            delay: 200
+            //        },
+            //        initialDelay: 500,
+            //        autoStart: false
+            //    });
+            $(".intro10-1")
+                .textillate({
+                    in: {
+                        effect: 'fadeIn',
+                        delay: 150
+                    },
+                    initialDelay: 500,
+                    autoStart: false
+                });
+            $(".intro11-0")
+                .textillate({
+                    in: {
+                        effect: 'fadeIn',
+                        delay: 150
+                    },
+                    initialDelay: 2500,
+                    autoStart: false
+                });
+
+            $(".intro12-0")
+                .textillate({
+                    in: {
+                        effect: 'fadeIn',
+                        delay: 150
+                    },
+                    initialDelay: 300,
+                    autoStart: false
+                });
+            $(".intro12-1")
+                .textillate({
+                    in: {
+                        effect: 'fadeIn',
+                        delay: 150
+                    },
+                    initialDelay: 1500,
+                    autoStart: false
+                });
+            $(".intro12-2")
+                .textillate({
+                    in: {
+                        effect: 'fadeIn',
+                        delay: 150
+                    },
+                    initialDelay: 2500,
+                    autoStart: false
+                });
+
+
+            $(".intro12-3")
+                .textillate({
+                    in: {
+                        effect: 'fadeIn',
+                        delay: 150
+                    },
+                    initialDelay: 3500,
+                    autoStart: false
+                });
+
+
+            $(".intro13-1")
+                .textillate({
+                    in: {
+                        effect: 'fadeIn',
+                        delay: 150
+                    },
+                    initialDelay: 1500,
+                    autoStart: false
+                });
+
+
+
+            $(".intro14-0")
+                .textillate({
+                    in: {
+                        effect: 'fadeIn',
+                        delay: 150
+                    },
+                    initialDelay: 0,
+                    autoStart: false
+                });
+
+            $(".intro14-1")
+                .textillate({
+                    in: {
+                        effect: 'fadeIn',
+                        delay: 150
+                    },
+                    initialDelay: 1500,
+                    autoStart: false
+                });
+
+            $(".intro14-2")
+                .textillate({
+                    in: {
+                        effect: 'fadeIn',
+                        delay: 150
                     },
                     initialDelay: 3000,
+                    autoStart: false
+                });
+
+
+
+            $(".intro14-4")
+                .textillate({
+                    in: {
+                        effect: 'fadeIn',
+                        delay: 150
+                    },
+                    initialDelay: 4500,
                     autoStart: false
                 });
 
@@ -154,83 +266,6 @@ $(document).ready(function () {
                     $(".intro1-4")
                         .textillate('start');
 
-                    /*
-                     $(".intro1-0")
-                     .addClass("animated slideInDown")
-                     .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-                     $(this).removeClass("animated slideInDown");
-                     });
-
-                     $(".intro1-1")
-                     .addClass("animated slideInLeft")
-                     .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-                     $(this).removeClass("animated slideInLeft");
-                     });
-
-                     $(".intro1-2")
-                     .addClass("animated fadeInDown")
-                     .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-                     $(this).removeClass("animated fadeInDown");
-                     });
-                     $(".intro3-0")
-                     .addClass("animated rotateIn")
-                     .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-                     $(this).removeClass("animated rotateIn");
-                     });
-                     $(".intro2-0")
-                     .addClass("animated fadeInUp")
-                     .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-                     $(this).removeClass("animated fadeInUp");
-                     });
-                     $(".intro2-1")
-                     .addClass("animated slideInLeft")
-                     .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-                     $(this).removeClass("animated slideInLeft");
-                     });
-
-
-
-                     $(".intro1-1")
-                     .css({opacity: 0.1, top: "20px", left: "20px", fontSize: "24px"})
-                     .transition({top: "50px", opacity: 1}, 2000,"ease");
-                     $(".intro1-2")
-                     .css({top: "100px", left: "-150px", fontSize: "45px", opacity: 0,fontWeight:"bold"})
-                     .transition({left: "20px", delay: 500, opacity: 1}, 2000, "ease");
-                     $(".intro1-3")
-                     .css({top: "150px", left: "20px", fontSize: "24px", opacity: 0})
-                     .transition({
-                     opacity: 1,
-                     perspective: "100px",
-                     rotateY: "20deg",
-                     fontSize: "26px",
-                     delay: 2000
-                     }, 1000, "ease");
-                     $(".intro1-4")
-                     .css({top: "150px", left: "120px", fontSize: "24px", opacity: 0})
-                     .transition({
-                     opacity: 1,
-                     perspective: "100px",
-                     rotateY: "-20deg",
-                     fontSize: "26px",
-                     delay: 2000
-                     }, 1000, "ease");
-
-
-                     $(".intro2-0")
-                     .css({bottom: "50px", right: 0, left: 100, opacity: 0, fontSize: "20px"})
-                     .transition({opacity: 1, fontSize: "22px", delay: 4000}, 300, "snap")
-                     .transition({scale:1.1}, 200, "snap")
-                     .transition({scale:1}, 400, "snap")
-                     .transition({scale:1.1}, 200, "snap")
-                     .transition({scale:1}, 400, "snap");
-                     $(".intro2-1")
-                     .css({left:0,bottom:"90px",opacity:0})
-                     .transition({opacity:1, left:"100px",delay:4000},1500,"snap");
-
-                     $(".intro3-0")
-                     .css({width:0,height:0,left:"50%",top:"50%",marginTop:"-40px",marginLeft:"-100px",backgroundSize:"0px 0px"})
-                     .transition({width:"200px",height:"150",rotate:"360deg",backgroundSize:"200px 150px",delay:3000},1000,"ease");
-                     */
                     break;
                 case 2:
 
@@ -346,8 +381,8 @@ $(document).ready(function () {
                 case 3:
 
                     $(".pic3").transition({
-                        y: "-250px",
-                        x: "150px"
+                        y: "-150px",
+                        x: "100px"
                     }, 100000, "linear");
 
                     $(".intro5-0").transition({
@@ -368,9 +403,6 @@ $(document).ready(function () {
                         y: "-350px",
                     }, 100000, "linear");
 
-
-                    $(".intro6-0")
-                        .textillate('start');
                     $(".intro7-0")
                         .textillate('start');
                     $(".intro7-1")
@@ -380,15 +412,73 @@ $(document).ready(function () {
 
                     $(".pic5").addClass("frame-move1");
 
-                    $(".intro8-0")
-                        .textillate('start');
+                    //$(".intro8-0")
+                    //    .textillate('start');
                     $(".intro8-1")
                         .textillate('start');
                     $(".intro9-0")
                         .textillate('start');
                     break;
                 case 6:
+
+                    $(".pic6").addClass("frame-move2");
+
+                    //$(".intro10-0")
+                    //    .textillate('start');
+                    $(".intro10-1")
+                        .textillate('start');
+                    $(".intro11-0")
+                        .textillate('start');
                     break;
+                case 7:
+                    $(".pic7").transition({
+                        opacity:1,delay:1500
+                    },1000,"linear");
+                    $(".pic8").transition({
+                        opacity:1,delay:2500
+                    },1000,"linear");
+                    $(".pic9").transition({
+                        opacity:1,delay:3500
+                    },1000,"linear");
+
+
+                    $(".intro12-0")
+                        .textillate('start');
+                    $(".intro12-1")
+                        .textillate('start');
+                    $(".intro12-2")
+                        .textillate('start');
+
+
+                    $(".intro12-3")
+                        .textillate('start');
+                    break;
+
+                case 8:
+
+                    $(".pic10").addClass("frame-move1");
+                    $(".pic10-container").transition({
+                        opacity:1,delay:3500
+                    },2500,"linear");
+
+                    //$(".intro8-0")
+                    //    .textillate('start');
+                    $(".intro13-1")
+                        .textillate('start');
+                    break;
+
+                case 9:
+                    $(".intro14 span.inner,.line1").textillate('start');
+                    $(".pic12").transition({
+                        opacity:1,delay:4000
+                    },2500,"linear");
+
+                    $(".line1").transition({
+                        opacity:1,delay:4000
+                    },1000,"linear");
+
+                    break;
+
             }
 
 
